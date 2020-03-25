3c4ed4daf07c7440059d8dc166680cefea58bc0d
Pedro Penacho <pedro.penacho@alphrtechnology.co.uk>
2020-03-25 17:32:37 +0000

    Putting all together and build the main class were application will run Program.cs

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SudokuSolver.Workers;
+using SudokuSolver.Strategies;
+
 
 namespace SudokuSolver
 {
@@ -10,6 +9,24 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
+            try
+            {
+                SudokuMapper sudokuMapper = new SudokuMapper();
+                SudokuBoardStateManager sudokuBoardStateManager = new SudokuBoardStateManager();
+                SudokuSolverEngine sudokuSolverEngine = new SudokuSolverEngine(sudokuBoardStateManager, sudokuMapper);
+                SudokuFileReader sudokuFileReader = new SudokuFileReader();
+                SudokuBoardDisplayer sudokuBoardDisplayer = new SudokuBoardDisplayer();
+
+                Console.WriteLine("Please enter the filename containing the Sudoku Puzzle:");
+                var filename = Console.ReadLine();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} {1}", "Sudoku puzzle cannot be solved because there was an error:", ex.Message);
+            }
         }
     }
 }
